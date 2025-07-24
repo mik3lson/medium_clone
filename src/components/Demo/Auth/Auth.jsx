a1f001a248b9e64bd0314a5732cbe088de8a342e
Michael Ndudi <ndudimichael06@gmail.com>
2025-07-24 02:12:00 +0100

    auth modal is added

--- src/components/Demo/Auth/Auth.jsx
+++ src/components/Demo/Auth/Auth.jsx
@@ -4,25 +4,41 @@ import {LiaTimesSolid} from 'react-icons/lia'
 import {FcGoogle} from 'react-icons/fc'
 import {MdFacebook} from 'react-icons/md'
 import {AiOutlineMail} from 'react-icons/ai'
+
+import SignIn from './SignIn'
+import SignUp from './SignUp'
+
 const Auth=()=> {
    const [createUser, setCreateUser] = useState(false);
+   const [signReq, setSignReq]= useState("sign-in");
+   const [modal, setModal] = useState(true);
+   const hidden = modal ? "visible opacity-100" :"invisible opacity-0"
     return(
-        <Modal>
+        <Modal modal ={modal} setModal={setModal} hidden ={hidden}>
             <section 
-                className ="z-50 fixed top-0 bottom-0 left-0 md:left-[10rem]
+                className ={`z-50 fixed top-0 bottom-0 left-0 md:left-[10rem]
                 overflow-auto right-0 md:right-[10rem] bg-white shadow
-            ">
-                <button className='absolute top-8 right-8 text-2xl hover:opacity-50'>
+                transition-all duration-500 
+                ${hidden}`}
+                >
+                <button 
+                   onClick = {() => setModal(false)}
+                   className='absolute top-8 right-8 text-2xl hover:opacity-50'>
                     <LiaTimesSolid/>
                 </button>
                 <div className='flex flex-col justify-center items-center gap-[3rem]'>
-                    <>
+                    {signReq === "" ? (
+                        <>
                     <h2 className='text-2xl pt-[5rem]'> {createUser ? "Join Medium" : "Welcome Back"}</h2>
  
                     <div className='flex flex-col gap-2 w-fit mx-auto'>
                         <Button icon= {<FcGoogle className='text-xl'/>} text={` ${createUser ? "Sign Up" :"Sign In"} with Google`} />
-                         <Button icon= {<MdFacebook className='text-xl text-blue-600 '/>}  text={` ${createUser ? "Sign Up" :"Sign In"} with Facebook` } />
-                          <Button icon= {<AiOutlineMail className='text-xl'/>}  text={` ${createUser ? "Sign Up": "Sign In"} with Email`} />
+                        <Button icon= {<MdFacebook className='text-xl text-blue-600 '/>}  text={` ${createUser ? "Sign Up" :"Sign In"} with Facebook` } />
+                        <Button 
+                            click ={() => setSignReq(createUser ? "sign-up" : "sign-in")}
+                            icon= {<AiOutlineMail className='text-xl'/>}  
+                            text={` ${createUser ? "Sign Up": "Sign In"} with Email`} 
+                        />
                         
                     </div>
                     <p> {createUser ? "Already have an acccount":"No account"}
@@ -33,8 +49,13 @@ const Auth=()=> {
                         </button>
                     </p>
                     </>
+                    ) : signReq === "sign-in" ? (
+                        <SignIn setSignReq = {setSignReq}/>
+                    ): signReq === "sign-up" ? (
+                        <SignUp setSignReq={setSignReq}/>
+                    ): null }
                     <p className ="md:w-[30rem] mx-auto text-center text-sm mb-[3rem]">
-                        Click 'Sign In' to agree to Medium's Terms of Service and acknowledge that Medium's privacy policy applies to you."
+                        Click "Sign In" to agree to Medium's Terms of Service and acknowledge that Medium's privacy policy applies to you."
                     </p>
                 </div>
             </section>
@@ -44,9 +65,11 @@ const Auth=()=> {
 export default Auth;
 
 
-const Button =({icon, text,click}) =>{
+const Button =({icon, text, click}) =>{
     return(
-      <button className='flex items-center gap-10 sm:w-[20rem] border border-black px-3 py-2 rounded-full'>
+      <button 
+         onClick = {click}
+         className='flex items-center gap-10 sm:w-[20rem] border border-black px-3 py-2 rounded-full'>
          {icon} {text}
       </button>
     );
