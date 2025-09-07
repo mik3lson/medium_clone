fad7fb85c9892067e7c9653cfa0a644c12f8631a
Michael Ndudi <ndudimichael06@gmail.com>
2025-09-07 16:00:04 +0100

    banner was added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import {Routes, Route} from "react-router-dom";
+import {Navigate, Routes, Route} from "react-router-dom";
 import Demo from "./components/Demo/Demo";
 import Home from "./components/home/home"; 
 import DemoHeader from './components/Demo/DemoHeader';
 import HomeHeader from './components/Home/HomeHeader';
 
 function App() {
-  const auth = false;
+  const currentUser = false;
     return (
       <>
-        {auth ? <HomeHeader/> : <DemoHeader />}
+        {currentUser ? <HomeHeader/> : <DemoHeader />}
        <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path ="/demo" element= {<Demo />}/>
+        {currentUser && <Route path="/" element={<Home />}/>}
+        {!currentUser &&<Route path ="/demo" element= {<Demo />}/>}
+        <Route path ="*" element = {<Navigate to ={ ! currentUser ? "/demo" :"/home"}/>}/>
       </Routes>
       </>
       
