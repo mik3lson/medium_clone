cbf770a509008a799a6a3c2ba7f0153a5738b81c
Michael Ndudi <ndudimichael06@gmail.com>
2025-09-08 16:15:12 +0100

    Demo page was added

---
+++ src/components/Common/Posts/Posts.jsx
@@ -0,0 +1,24 @@
+import react from "react";
+
+const Posts =()=>{
+    return(
+        <div>
+            <h1>Posts</h1>
+             <h1>Posts</h1>
+             <h1>Posts</h1>
+              <h1>Posts</h1>
+             <h1>Posts</h1>
+              <h1>Posts</h1>
+              <h1>Posts</h1>
+              <h1>Posts</h1>
+              <h1>Posts</h1>
+              <h1>Posts</h1>
+              <h1>Posts</h1>
+              <h1>Posts</h1>
+              <h1>Posts</h1>       
+
+        </div>
+    );
+}
+
+export default Posts;
